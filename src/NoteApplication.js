639518b23886a4209c54cc71c7e8a01a05907adf
richardekong <richardekong@gmail.com>
2016-05-19 11:44:22 +0100

    Remove duplicates

--- src/NoteApplication.js
+++ src/NoteApplication.js
@@ -9,7 +9,7 @@ function note(author)
 	this.author=author;
 	this.noteList=[];
 
-this.createNote=function(note_content)
+this.create=function(note_content)
 {
 	this.noteList.push(note_content);
 };
@@ -39,15 +39,19 @@ this.search=function(search_text)
 		{
 			
 			buffer.push(this.get(note_id));
+			break;
 		}
 	}
-			return "Showing results for search "+ search_text+"\n\n"+"Note_Id: "+note_id+"\n"+buffer.toString()+"\n"+"By Author "+ this.author;
-		//	console.log(buffer);
+	
+			return "Showing results for search "+ search_text+"\n\n"+"Note_Id: "+(note_id)+"\n"+buffer+"\n"+"By Author "+ this.author+"\n\n";
+		
 };
 this.delete=function(note_id)
 {
-	
+	    var bin=[];
+	    bin.push(this.get(note_id));
 		this.noteList.splice(note_id,1);
+		console.log(bin.toString()+" has been deleted"+"\n"+"\n"+"Note List Remains...");
 };
 this.edit=function(note_id, new_content)
 {
@@ -56,13 +60,18 @@ this.edit=function(note_id, new_content)
 }
 }
 
-var myNote=new note("Richard Ekong");
-myNote.createNote("I love what I do");
-myNote.createNote("Commercial Law");
-myNote.createNote("World at war")
-myNote.createNote("Sleepy Harrows")
+/**var myNote=new note("Richard Ekong");
+myNote.create("I love what I do");
+myNote.create("Commercial Law");
+myNote.create("World at war")
+myNote.create("Sleepy Harrows");
+myNote.create("The Andelian");
+myNote.create("The Boot-camp")
 myNote.listNote();
 //myNote.delete(1);
 console.log("After deleting "+myNote.noteList[1]+"\n\n");
 myNote.listNote();
-console.log(myNote.search("Sleepy"));
+console.log(myNote.search("An"));
+myNote.delete(3);
+
+myNote.listNote();**/
